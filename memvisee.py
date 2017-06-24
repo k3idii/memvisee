4500316d294403dee1252be0f139d5be8a79bc81
keidii <keidii@gmail.com>
2017-06-24 02:45:38 +0200

    fix 1.

--- memvisee.py
+++ memvisee.py
@@ -1,24 +1,27 @@
 import pygame
 import time
-
-SCREEN_SIZE = [800, 800]
+import argparse
 
 KEYMAP = {}
 
 def add_key_map(key, func):
-  # global KEYMAP
+  """ adds entry in global keymap dict """
   KEYMAP[key] = func
 
 def keyz(keys):
+  """ convert strin-of-chars into list-of-integers """
   return map(ord, keys)
 
 def hotkey(arg):
+  """ add hotkey(s) to KEYMAP list """
   def _wrap(func):
     for key in arg:
       add_key_map(key, func)
     return func
   return _wrap
 
+def bit2raw(bits):
+  return int(''.join(map(str,bits)),2)
 
 class MasterClass(object):
   file_handle = None
@@ -28,41 +31,79 @@ class MasterClass(object):
   the_screen = None
   keep_working = True
   need_redraw = True
-  data_format = 'RGB'
-  kbd = {}
+
+  kbd_state = {}
+  
   step_small_horiz = 1
   step_big_horiz = 33
   step_small_vert = 11
   step_big_vert = 500000
 
+  x_space = 10
+  buffer = []
 
-  def __init__(self, fn):
-    self.file_handle = open(fn, 'rb')
+  put_pixel = None # func 
+
+
+  def __init__(self, filename=None, bit_per_pixel=24, skip=0, xres=800, yres=800, pix_size=1):
+    self.file_name = filename
+    self.file_handle = open(filename, 'rb')
     self.file_offset = 0x00
+    if bit_per_pixel < 1:
+      bit_per_pixel = 1
+    self.bit_per_pixel = bit_per_pixel
+    self.skip = skip
+    if xres < 100:
+      xres = 100
+    if yres < 100:
+      yres = 100
+    self.xres = xres
+    self.yres = yres
+    if pix_size < 1:
+      pix_size = 1
+    self.pix_size = pix_size
+    self.the_screen = pygame.display.set_mode([self.xres * pix_size, self.yres * pix_size])
+    if pix_size == 1:
+      self.put_pixel = self._put_pixel_1
+    else:
+      self.put_pixel = self._put_pixel_n
+
+    self.max_color = 2 << (self.bit_per_pixel - 1)
+    self.color_coef = 0xFFffFF / (self.max_color-1.0)
+    self.make_color = self._make_color_rgb
+    if self.bit_per_pixel == 24:
+        self.make_color = self._make_color_24
+
+    print self.bit_per_pixel % 8
+    if self.bit_per_pixel % 8 == 0:
+      self.get_next_piexl = self._get_next_8
+      self.chunk_size = self.bit_per_pixel / 8
+    else:
+      self.get_next_piexl = self._get_next_x
     self.keep_working = True
-    self.the_screen = pygame.display.set_mode(SCREEN_SIZE)
+
+
 
   def start(self):
     while self.keep_working:
       time.sleep(0.01)
       for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
-          print " !! QUIT !!"
-          self.stop()
-          return
+          print(" !! QUIT !!")
+          return self.stop()
         if ev.type == pygame.KEYDOWN:
           key = ev.key
-          self.kbd[key] = 1
-          #print "KEYPRES " ,key
+          self.kbd_state[key] = 1
+          # print "KEYPRES " ,key
         if ev.type == pygame.KEYUP:
           key = ev.key
           try:
-            del self.kbd[key]
+            del self.kbd_state[key]
             #print "KEYUP", key
           except Exception as err:
-            print "Fail to release key: ", key, "reason:", str(err)
+            print("Fail to release key: {0} reason:{1}".format(str(key), str(err)))
 
-      for key in self.kbd:
+      for key in self.kbd_state:
         action = KEYMAP.get(key, None)
         if action and callable(action):
           action(self)
@@ -74,116 +115,131 @@ class MasterClass(object):
         pygame.display.flip()
         self.need_redraw = False
 
-  def rd(self):
+  def repaint(self):
     self.need_redraw = True
 
   @hotkey(keyz("qQ"))
   def stop(self):
-    print "WILL STOP keep_workingING !"
+    print("WILL STOP keep_workingING !")
     self.keep_working = False
 
   @hotkey([61])
   def inc_row_size(self):
-    self.rd()
+    self.repaint()
     self.pix_per_row += self.step_big_horiz
+    if self.pix_per_row >= self.xres:
+      print("XRES LIMIT !")
+      self.pix_per_row = self.xres - 1
+
 
   @hotkey([45])
   def dec_row_size(self):
-    self.rd()
+    self.repaint()
     self.pix_per_row -= self.step_big_horiz
     if self.pix_per_row < 1:
       self.pix_per_row = 1
 
   @hotkey([281])
   def go_down(self):
-    self.rd()
+    self.repaint()
     self.file_offset += self.step_big_vert
 
   @hotkey([280])
   def go_up(self):
-    self.rd()
+    self.repaint()
     self.file_offset -= self.step_big_vert
     if self.file_offset < 0:
       self.file_offset = 0
 
   @hotkey([273])
   def go_up_1(self):
-    self.rd()
+    self.repaint()
     self.file_offset -= self.step_small_vert
     if self.file_offset < 0:
       self.file_offset = 0
 
   @hotkey([274])
   def go_down_1(self):
-    self.rd()
+    self.repaint()
     self.file_offset += self.step_small_vert
 
   @hotkey([276])
   def go_left_1(self):
-    self.rd()
+    self.repaint()
     self.pix_per_row -= self.step_small_horiz
     if self.pix_per_row < 1:
       self.pix_per_row = 1
 
   @hotkey([275])
   def go_right_1(self):
-    self.rd()
+    self.repaint()
     self.pix_per_row += self.step_small_horiz
 
-  @hotkey(keyz('Ff'))
-  def set_format(self):
-    print
-    print "ENTER NEW FORMAT STRING (RBG ... ) :"
-    tmp = raw_input()
-    print "New format : ", `tmp`
-    self.data_format = tmp
-    self.rd()
-
-  def go_to_pos(self):
+  def go_to_pos(self): ## TODO <- implement me
     # read from user
-    self.rd()
+    self.repaint()
 
+  def _put_pixel_1(self, posx, posy, color): ## putpixel for 1px
+    self.the_screen.set_at((posx, posy), color)
+
+  def _put_pixel_n(self, posx, posy, color): ## generic putpixel
+    self.the_screen.fill(color, rect=(posx*self.pix_size, posy*self.pix_size, self.pix_size, self.pix_size))
+
+  def _make_color_rgb(self, idx):
+    return int(idx * self.color_coef)
+
+  def _make_color_24(self, idx):
+    return idx
+
+  def _get_next_8(self):
+    tmp = self.file_handle.read(self.chunk_size)
+    self.file_handle.read(self.skip)
+    if len(tmp) != self.chunk_size:
+      raise Exception("EOF HIT1")
+    return self.make_color(int(tmp.encode('hex'),16))
+
+  def _get_next_x(self):
+    while len(self.buffer) < self.bit_per_pixel:
+      one_byte = self.file_handle.read(1)
+      if len(one_byte) != 1 or one_byte is None:
+        raise Exception("EOF HIT2")
+      self.buffer.extend(list(bin(ord(one_byte))[2:].rjust(8, '0')))
+    tmp = self.buffer[:self.bit_per_pixel]
+    self.buffer = self.buffer[self.bit_per_pixel+self.skip:]
+    return self.make_color(bit2raw(tmp))
 
   def update_screen(self):
     self.file_handle.seek(self.file_offset)
-    chunk_size = len(self.data_format)
-    def put_pix(x, y, color):
-      self.the_screen.set_at((x, y), color)
-    pos = {}
-    for c in 'RGB':
-      pos[c] = self.data_format.index(c)
-
-    row_and_space = self.pix_per_row + 20
-    #              .-- 700 = 800 -100 lol ! get_SCREEN_SIZE(moron)
-    num_stripes = 700 / row_and_space
-
-    if num_stripes < 1:
-      num_stripes = 1
+    col_size = self.pix_per_row + self.x_space
+    col_count = self.xres / col_size ## int rount will do the job
+    t1 = time.time()
     try:
-      t1 = time.time()
-      for ss in range(num_stripes):
-        pad = ss * row_and_space
-        for row in range(self.max_rows):
-          for col in range(self.pix_per_row):
-            ch = map(ord, self.file_handle.read(chunk_size))
-            put_pix(pad + col, row, (ch[pos['R']], ch[pos['G']], ch[pos['B']]))
-      dt = time.time() - t1
-      print "Rendered in ", dt, " seconds "
+      for col_no in range(col_count):
+        pad = col_no * col_size
+        for row in range(self.yres):
+          for pix_x in range(self.pix_per_row):
+            self.put_pixel(pad + pix_x, row, self.get_next_piexl())  
     except Exception as err:
-      print "Error : ", str(err)
-
+      print("Fail to render : {0}".format(str(err)))
+    dt = time.time() - t1
+    print("Rendered in {0} seconds ".format(dt))
 
 def main():
-  import sys
-  if len(sys.argv) != 2:
-    print "Usage : %s file.dump , use --help for help ;-)" % sys.argv[0]
+  parser = argparse.ArgumentParser(description='MVS == MEMory VIsual SEEker :-) ')
+  parser.add_argument("filename", action="store", help="Data file name")
+  parser.add_argument("--bps", action="store", dest="bit_per_pixel", default=24, type=int, help="bits per pixel")
+  parser.add_argument("--skip", action="store", default=0, type=int, help="Skipt N bits after pixel ... Usefull to skip alpha")
+  parser.add_argument("--xres", action="store", default=800, type=int, help="GUI X-size of window (in pix_size), def=800")
+  parser.add_argument("--yres", action="store", default=800, type=int, help="GUI Y-size of window (in pix_size), def=800")
+  parser.add_argument("--pix-size", action="store", default=1, type=int, help="Pixel size (square), def=1")
+  opts = parser.parse_args()
+  print opts
+  try:
+    obj = MasterClass(**vars(opts))
+  except Exception as ex:
+    print("Fail to setup, reason : [{0}]".format(str(ex)))
     return
-  if sys.argv[1] == '--help':
-    print "Keys:"
-  filename = sys.argv[1]
-  obj = MasterClass(filename)
   obj.start()
-
 
 
 if __name__ == '__main__':
